39c33d8411f5bec15d1f48b1db01a6d02baab871
Eugene Aliakberov <eugenealiakberov@gmail.com>
2026-04-12 11:19:21 +0300

    HW6

--- modules/listeners.js
+++ modules/listeners.js
@@ -1,12 +1,19 @@
 import { comments, updateComments } from "./comments.js";
-import { renderLike, renderComments } from "./renders.js";
-import { commentsEl } from "./renders.js";
+import { renderLike, fetchAndRenderComments } from "./fetchAndRender.js";
+import { commentsEl } from "./fetchAndRender.js";
+
+function delay(interval = 900) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, interval);
+    });
+}
 
 export const initListeners = () => {
     const commentEls = commentsEl.querySelectorAll(".comment");
-    const nameEl = document.querySelector(".add-form-name");
+
     const textEl = document.querySelector(".add-form-text");
-    const addBtnEl = document.querySelector("button.add-form-button");
 
     for (let commentEl of commentEls) {
         const likeBtn = commentEl.querySelector(".like-button");
@@ -15,7 +22,7 @@ export const initListeners = () => {
         //Клик по самому комменту
         commentEl.addEventListener("click", () => {
             textEl.value =
-                `>${comments[index].name}\n"${comments[index].text}"\n`
+                `>${comments[index].author.name}\n"${comments[index].text}"\n`
                     .replaceAll("&gt;", ">")
                     .replaceAll("&lt;", "<");
         });
@@ -23,18 +30,32 @@ export const initListeners = () => {
         //Клик по лайку внутри коммента и вызов перерисовки только лайков
         likeBtn.addEventListener("click", (event) => {
             event.stopPropagation();
-            if (comments[index].isliked) {
-                comments[index].isliked = false;
-                comments[index].likes--;
-            } else {
-                comments[index].isliked = true;
-                comments[index].likes++;
-            }
-            renderLike(commentEl, index);
+            likeBtn.classList.add("-loading-like");
+            delay()
+                .then(() => {
+                    if (comments[index].isliked) {
+                        comments[index].isliked = false;
+                        comments[index].likes--;
+                    } else {
+                        comments[index].isliked = true;
+                        comments[index].likes++;
+                    }
+                    renderLike(commentEl, index);
+                })
+                .then(() => {
+                    likeBtn.classList.remove("-loading-like");
+                });
         });
     }
+};
 
-    //Нажатие на кнопку
+//Нажатие на кнопку
+export const initAddListener = () => {
+    const textEl = document.querySelector(".add-form-text");
+    const nameEl = document.querySelector(".add-form-name");
+    const addBtnEl = document.querySelector("button.add-form-button");
+    const addForm = document.querySelector(".add-form");
+
     addBtnEl.addEventListener("click", () => {
         if (nameEl.value.trim() === "" || textEl.value.trim() === "") {
             return;
@@ -51,20 +72,18 @@ export const initListeners = () => {
                 .trim(),
         };
 
+        addForm.style = "display: none";
+        commentsEl.innerHTML += `<div style = "text-align: center;">Подождите, комментарий добавляется</div>`;
+
         fetch("https://wedev-api.sky.pro/api/v1/:ealiakberov/comments", {
             method: "POST",
             body: JSON.stringify(newComment),
-        }).then(() => {
-            return fetch(
-                "https://wedev-api.sky.pro/api/v1/:ealiakberov/comments",
-            )
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    updateComments(data.comments);
-                    renderComments();
-                });
-        });
+        })
+            .then(() => {
+                return fetchAndRenderComments();
+            })
+            .then(() => {
+                addForm.style = "";
+            });
     });
 };
